af2c511c9ca0a4353650e3954f3c35577b0d9b56
Oliver Hynds <oliver.hynds@gmail.com>
2018-01-10 15:56:09 +0000

    Update A_GLOBALS.ino

--- program/A_GLOBALS.ino
+++ program/A_GLOBALS.ino
@@ -1,44 +1,44 @@
-void setup() {
-  lcd.createChar(0, retarrow);
-  pinMode(CLK, INPUT);
-  pinMode(DT, INPUT);
+#include <Wire.h>
+#include <LCD.h>
+#include <LiquidCrystal_I2C.h>
 
-  pinMode(switch, INPUT_PULLUP);
+#define DT 3 //for rot
+#define CLK 4 //for rot
 
-  lcd.begin(16, 2);
+#define switch  2
 
-  // Switch on the backlight
-  lcd.setBacklightPin(BACKLIGHT_PIN, POSITIVE);
-  lcd.setBacklight(HIGH);
-  lcd.home();  // go home
+#define I2C_ADDR 0x27  // Define I2C Address where the PCF8574A is
+#define BACKLIGHT_PIN 3 //defs for lcd
+#define En_pin 2
+#define Rw_pin 1
+#define Rs_pin 0
+#define D4_pin 4
+#define D5_pin 5
+#define D6_pin 6
+#define D7_pin 7
 
-  attachInterrupt (digitalPinToInterrupt(3),encoderisr,FALLING); //rotary encoder isr
-  attachInterrupt (digitalPinToInterrupt(2),switchisr,RISING); //switch isr
+uint8_t retarrow[8] = {0x1, 0x1, 0x5, 0x9, 0x1f, 0x8, 0x4}; //custom char for lcd
 
-}
 
-void loop() {
-  if (turndetected) {
-    if (up) {
-      encoderPOS++;
-    } else {
-      encoderPOS--;
-    }
-    turndetected = false;
-  }
-  
-  lcd.clear();
 
-  if(switchstate == HIGH){ //button mode
-    lcd.setCursor(0, 0);
-    lcd.write(126);  
-  }else{
-    lcd.setCursor(0, 1);
-    lcd.write(126); 
-  }
-  
-  lcd.setCursor(1, 0);
-  lcd.print(programs[encoderPOS % numprograms]);
+String programs[] = {"wave", "static", "rainbow", "droplets", "random"};
+/*                     |        |          |          |           |
+                       |        |          |          |           \---*/ String random_modes[] = {"1", "2", "3", "4", "5"}; /*
+                       |        |          |          |
+                       |        |          |          \--- */ String droplets_mode[] = {"mono cromatic", "colourful", "rising", "dense", "sparse"}; /*
+                       |        |          |
+                       |        |          \--- */ String rainbow_mode[] = {"pulse", "vertical", "horizontal", "whole colour", "random"}; /*
+                       |        |
+                       |        \---- */ String static_mode[] = {"red", "green", "blue", "purple", "yellow"}; /*
+                       |
+                       \---- */ String wave_mode[] = {"wavey", "really wavey", "supper wavey", "something", "something"}; /*
+                       
+*/
+const int numprograms = 5;
 
-  delay(10);
-}
+
+volatile bool turndetected; //var for rot isr
+volatile bool up; //var for rot idr
+unsigned short int encoderPOS = 0; //total possision for rot 
+
+volatile bool switchstate; //ver for switch isr
